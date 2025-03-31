9348d90dfd8e63d58fbb385efc85ff7a9d382382
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-03-31 16:33:58 +0700

    Finish fetching weather data functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./styles.css";
+
+const API_KEY = 'D82Z94FJ9Q7CBMRB5RQH2M26U';
+
+function toCelsius(temp) {
+    return (Math.round((temp - 32) * 5 / 9 * 10) / 10).toString();
+}
+
+async function getWeatherData(location) {
+    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
+
+    try {
+        const response = await fetch(url, {mode: 'cors'});
+        const weatherData = await response.json();
+
+        const location = weatherData.address;
+        const tempF = weatherData.currentConditions.temp;
+        const tempC = toCelsius(Number(tempF));
+        const feelsLike = weatherData.currentConditions.feelslike;
+        const humidity = weatherData.currentConditions.humidity;
+        const windSpeed = weatherData.currentConditions.windspeed;
+        const icon = weatherData.currentConditions.icon;
+
+        console.log("Location: " + location);
+        console.log("Temp: " + tempF + "F / " + tempC +"C");
+        console.log("Feels Like: " + feelsLike + "F / " + toCelsius(Number(feelsLike)) +"C");
+        console.log("Humidity: " + humidity + '%');
+        console.log("Wind speed: " + windSpeed + " m/s");
+        console.log("Icon: " + icon);
+    } catch (error) {
+
+    }
+}
+
+getWeatherData('bangkok');
